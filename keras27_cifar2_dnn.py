e70c7af773275d5b2d4b1517fa9640cb648ae4d9
Sohyun617 <bsohyun8@naver.com>
2020-02-20 23:10:40 +0900

    200220

---
+++ keras27_cifar2_dnn.py
@@ -0,0 +1,47 @@
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from keras.callbacks import EarlyStopping
+import numpy as np 
+
+(x_train, y_train),(x_test, y_test) = cifar10.load_data()
+#(32 ,32, 3)
+# print(x_train)
+# print(y_train)
+# print(x_train.shape) #(50000,32,32,3)
+# print(y_train.shape) #(50000,1)
+
+x_train = x_train.reshape(x_train.shape[0],32*32*3).astype('float32')/255
+x_test = x_test.reshape(x_test.shape[0],32*32*3).astype('float32')/255
+
+from keras.utils import np_utils
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+# print(x_train.shape)
+# print(y_train.shape)
+
+from keras.models import Sequential
+from keras.layers import Dense
+model = Sequential()
+
+model.add(Dense(32, input_shape =(32*32*3,))) 
+model.add(Dense(16))
+model.add(Dense(10))
+model.add(Dense(10, activation='softmax'))
+
+#model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+early_stopping = EarlyStopping(monitor='loss', patience=20)
+
+model.fit(x_train,y_train, validation_split=0.2,
+          epochs=50, batch_size=8, verbose=1, 
+          callbacks=[early_stopping])
+
+acc = model.evaluate(x_test, y_test, batch_size=8)
+print('accuracy: ',acc)
+
