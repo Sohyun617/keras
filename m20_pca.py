41769136da21de14349eea55a441536ae5f0527d
Sohyun617 <bsohyun8@naver.com>
2020-03-22 08:19:30 +0900

    200322

---
+++ m20_pca.py
@@ -0,0 +1,15 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+scaler = StandardScaler()
+scaler.fit(cancer.data)
+x_scaled = scaler.transform(cancer.data)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components= 10)
+pca.fit(x_scaled)
+
+x_pca = pca.transform(x_scaled)
+print("원본 데이터 형태: ", x_scaled.shape)
+print("축소된 데이터 형태: ", x_pca.shape)
